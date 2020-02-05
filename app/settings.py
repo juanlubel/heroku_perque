821a10c2a288b21a7f73e47e6e8df43e770f02eb
Juanlu Belda <juanluis.belda@gmail.com>
2020-02-05 20:08:04 +0100

    four

--- app/settings.py
+++ app/settings.py
@@ -88,7 +88,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASE['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
