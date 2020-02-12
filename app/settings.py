66d61c1c8c3bc218551797fe920857ec873aea81
Juanlu Belda <juanluis.belda@gmail.com>
2020-02-12 17:59:20 +0100

    whitenoise

--- app/settings.py
+++ app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -82,6 +83,10 @@ TEMPLATES = [
         },
     },
 ]
+
+MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+)
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
